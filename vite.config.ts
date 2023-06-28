36e300cb502cf440ef0fbbe648226c7110be6408
hans <hans@twjoin.com>
2023-06-28 14:43:32 +0800

    feat(lint-staged): add husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import autoprefixer from 'autoprefixer';
-import postcss from 'postcss';
-
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+});
