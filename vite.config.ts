66e8e63d03dd511a93c09515465f2aedd9ccd69a
hans <hans@twjoin.com>
2023-06-29 11:12:42 +0800

    feat(vite.config.js): add config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,51 @@
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
+import SvgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [
+    vue(),
+    UnoCSS(),
+    SvgLoader({
+      svgoConfig: {
+        plugins: [
+          "removeXMLNS",
+          {
+            name: "removeAttrs",
+            params: {
+              attrs: "fill",
+            },
+          },
+          {
+            name: "addAttributesToSVGElement",
+            params: {
+              attribute: {
+                fill: "#AAADB3",
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+      output: {
+        entryFileNames: `assets/main.[hash].js`,
+        chunkFileNames: `assets/[hash].js`,
+        assetFileNames: `assets/[hash].[ext]`,
+      },
+    },
+  },
 });
